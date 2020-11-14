613d3eb79a9573c732e541abec63b23fa5d61077
pontus.horn <pontus.horn@gmail.com>
2020-11-14 23:50:35 +0100

    Add basic name/office filter

--- src/NinjasGrid.svelte
+++ src/NinjasGrid.svelte
@@ -1,21 +1,50 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import NinjaCard from './NinjaCard.svelte';
+  import Paper from './Paper.svelte';
+  import { Office } from './types';
   import type { Ninja } from './types';
 
   const NINJAS_API_URL = `https://api.tretton37.com/ninjas`;
   let ninjas: Ninja[] = [];
+  let nameFilter: string = '';
+  let officeFilter: Office;
 
   onMount(async () => {
     const response = await fetch(NINJAS_API_URL);
-    const body = await response.json();
+    ninjas = await response.json();
+  });
 
-    // Only use a few profiles to begin with for development
-    ninjas = body.slice(0, 12);
+  $: filteredNinjas = ninjas.filter((ninja) => {
+    if (
+      nameFilter &&
+      !ninja.name.toLocaleLowerCase().includes(nameFilter.toLocaleLowerCase())
+    ) {
+      return false;
+    }
+
+    if (officeFilter && ninja.office !== officeFilter) {
+      return false;
+    }
+
+    return true;
   });
 </script>
 
 <style>
+  main {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  .filters {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    padding: 1rem;
+  }
+
   .grid {
     display: grid;
     gap: 1rem;
@@ -31,10 +60,26 @@
   }
 </style>
 
-<div class="grid">
-  {#each ninjas as ninja}
-    <NinjaCard {ninja} />
-  {:else}
-    <p>Loading...</p>
-  {/each}
-</div>
+<main>
+  <Paper>
+    <div class="filters">
+      <label>Name: <input bind:value={nameFilter} /></label>
+      <label>Office:
+        <select bind:value={officeFilter}>
+          <option value={undefined}>Any office</option>
+          {#each Object.values(Office) as office}
+            <option value={office}>{office}</option>
+          {/each}
+        </select>
+      </label>
+    </div>
+  </Paper>
+
+  <div class="grid">
+    {#each filteredNinjas as ninja (ninja.email)}
+      <NinjaCard {ninja} />
+    {:else}
+      <p>Loading...</p>
+    {/each}
+  </div>
+</main>
